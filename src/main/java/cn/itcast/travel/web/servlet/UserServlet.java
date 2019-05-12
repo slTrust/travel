430a832595b3592a066f6686e34cac06e564d8c6
hjx <trustfor@sina.cn>
2019-05-12 23:37:26 +0800

    UserServlet 的路径更改
    UserServlet 的路径更改

--- src/main/java/cn/itcast/travel/web/servlet/UserServlet.java
+++ src/main/java/cn/itcast/travel/web/servlet/UserServlet.java
@@ -1,22 +1,211 @@
 package cn.itcast.travel.web.servlet;
 
+import cn.itcast.travel.domain.ResultInfo;
+import cn.itcast.travel.domain.User;
+import cn.itcast.travel.service.UserService;
+import cn.itcast.travel.service.impl.UserServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.beanutils.BeanUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+import java.util.Set;
 
 /*
 匹配 /user/add    /user/find
 */
 @WebServlet("/user/*")
 public class UserServlet extends BaseServlet {
-    public void add(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("userServlet的 add方法");
+    // 声明UserService业务对象
+    private UserService service = new UserServiceImpl();
+
+
+    /**
+     * 注册方法
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void regist(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("userServlet的 regist方法");
+        //0 校验验证码
+        String check = request.getParameter("check");
+        // 从 session里获取 验证码
+        HttpSession session = request.getSession();
+        String checkcode_server = (String) session.getAttribute("CHECKCODE_SERVER");
+        // 清空 session里的验证码 为了保证验证码只能使用一次
+        session.removeAttribute("CHECKCODE_SERVER");
+        // 比较
+        if (checkcode_server == null || !checkcode_server.equalsIgnoreCase(check)) {
+            // 校验码错误
+            ResultInfo info = new ResultInfo();
+            info.setFlag(false);
+            info.setErrorMsg("验证码错误");
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(info);
+            response.setContentType("application/json;charset=utf-8");
+            response.getWriter().write(json);
+            return;
+        }
+
+
+        //1.获取数据
+        Map<String, String[]> map = request.getParameterMap();
+        Set<String> keySet = map.keySet();
+        for (String key : keySet) {
+            String value = map.get(key)[0];
+            System.out.println(value);
+        }
+        //2.封装对象
+        User user = new User();
+        try {
+            BeanUtils.populate(user, map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        //3.调用service完成注册
+        System.out.println(user);
+        boolean flag = service.regist(user);
+        //4.响应结果
+        ResultInfo info = new ResultInfo();
+        if (flag) {
+            // succ
+            info.setFlag(true);
+        } else {
+            // fail
+            info.setFlag(false);
+            info.setErrorMsg("注册失败");
+        }
+        //5.将 info对象序列化为json
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(info);
+        // 将json写回客户端
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write(json);
     }
 
-    public void find(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("userServlet的 find方法");
+    /**
+     * 登陆功能
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("userServlet的 login 方法");
+        //1.获取用户名和密码数据
+        Map<String, String[]> map = request.getParameterMap();
+        //2.封装User对象
+        User user = new User();
+        try {
+            BeanUtils.populate(user, map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        //3.调用Service 查询
+        User u = service.login(user);
+
+        ResultInfo info = new ResultInfo();
+
+        //4.判断用户对象是否为null
+        if (u == null) {
+            // 用户名密码错误
+            info.setFlag(false);
+            info.setErrorMsg("用户名或密码错误");
+        }
+        //5. 用户是否激活
+        if (u != null && "N".equals(u.getStatus())) {
+            // 用户尚未激活
+            info.setFlag(false);
+            info.setErrorMsg("您尚未激活，请激活");
+        }
+
+        //6. 判断登陆成功
+        if (u != null && "Y".equals(u.getStatus())) {
+            //登陆成功
+            request.getSession().setAttribute("user", u);//登录成功标记
+
+            info.setFlag(true);
+        }
+        System.out.println(u);
+
+        // 响应数据
+        ObjectMapper mapper = new ObjectMapper();
+
+        response.setContentType("application/json;charset=utf-8");
+        mapper.writeValue(response.getOutputStream(), info);
     }
+
+    /**
+     * 查询单个对象
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void findOne(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 从 session中获取登陆用户
+        Object user = request.getSession().getAttribute("user");
+        // 将user写回客户端
+
+        ObjectMapper mapper = new ObjectMapper();
+        response.setContentType("application/json;charset=utf-8");
+        mapper.writeValue(response.getOutputStream(),user);
+    }
+
+    /**
+     * 退出
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void exit(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //1.销毁 session
+        request.getSession().invalidate();
+        //2. 跳转页面
+        response.sendRedirect(request.getContextPath() + "/login.html");
+    }
+
+    /**
+     * 激活
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void active(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //1.获取激活码
+        String code = request.getParameter("code");
+        if(code!=null) {
+            //2.调用激活操作
+            boolean flag = service.active(code);
+            //3. 判断标记
+            String msg = null;
+            if (flag) {
+                // succ
+                msg = "激活成功，请<a href='login.html'>登录</a>";
+            } else {
+                // fail
+                msg = "激活失败，请联系管理员";
+            }
+            response.setContentType("text/html;charset=utf-8");
+            response.getWriter().write(msg);
+        }
+    }
+
 }
